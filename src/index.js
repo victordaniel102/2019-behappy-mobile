2c0f7142d542cbd5bb2a389943f44e6c0d858b39
Daniel Victor <daniel.victor9@hotmail.com>
2019-11-07 10:19:41 -0300

    First app update

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <Text>Meu App #DCL</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center"
+    }
+});
